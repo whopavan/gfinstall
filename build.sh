7339de223bd0599f2e6531acd88026d604c60eba
Pavan Kumar <pavank@sostronk.com>
2021-10-09 15:09:22 +0530

    Do not allow users to update google fonts list

--- build.sh
+++ build.sh
@@ -33,6 +33,7 @@ makeDeb () {
 }
 
 buildDebSource () {
+    updateGFList
     makeDeb
     debuild -S
     read -p "Do you want to upload Package to PPA? (Y/N): " wantUpload
@@ -50,6 +51,7 @@ buildDebBinary () {
                 exit;;
             * ) exit;;
     esac
+    updateGFList
     makeDeb
     debuild
 }
@@ -71,6 +73,50 @@ clean () {
     rm -f ../gfinstall_*
 }
 
+fetchLatestGF() {
+    # This function updates gflist with all google fonts available
+    # To use this you must set GOOGLE_API_KEY env variable
+
+    # In the first time `apiData=` we get data from the api
+    # Then we parse api data and copy it to gflist file
+    #       - `echo ${apiData,,}` will convert everything to lowercase
+    #       - `jq -r` is responsible for parsing api data (keep in mind that all data have been lowered before parsing,
+    #         might be bad but it stays till I find better solution)
+    #       - `tr -d ' '` will get rid of all the spaces in between names and write the output to gflist file
+
+    apiData=`curl -s "https://www.googleapis.com/webfonts/v1/webfonts?key=$GOOGLE_API_KEY&sort=alpha"`
+    echo ${apiData,,} | jq -r '.items[].family' | tr -d ' ' > fonts_list/gflist
+
+    if [ $? != 0 ]
+	then
+		return 1
+	fi
+    return 0
+}
+
+updateGFList() {
+	echo "Updating google fonts list"
+    fetchLatestGF
+
+    if [ $? != 0 ]
+	then
+		printFailed "Failed to fetch latest google fonts"
+		return 1
+	fi
+
+	newFontsList=`echo $(cat fonts_list/gflist)`
+	sed -i '/^fontsList=*/c\fontsList="'"$newFontsList"'"' 'gfinstall.bash'
+	
+	if [ $? != 0 ]
+	then
+		printFailed "Failed to update google fonts list"
+		return 1
+	fi
+
+	echo "Updated Google Fonts List"
+	return 0
+}
+
 uploadPPA () {
     echo "Current version: $fullCurVer"
     read -p "Is the current version correct? (Y/N): " verCheck
@@ -88,18 +134,20 @@ then
     currentVersion
     PARAM=$1
     case $PARAM in
-        -ds | --debSource ) buildDebSource;
-                            exit;;
-        -db | --debBinary ) buildDebBinary;
-                            exit;;
-        -c  | --clean     ) clean;
-                            exit;;
-        -d  | --debug     ) debug;
-                            exit;;
-        -u | --uploadPPA  ) uploadPPA;
-                            exit;;
-                        * ) echo -e "Usage:\n-ds | --debSource:\t Generates debian source file\n-db | --debBinary:\t Generates debian binary file\n-sn | --snap:\t\t Generates Snap Package\n-c | --clean:\t\t Cleans directory, removes all build files etc\n-u | --uploadPPA:\t Uploads source file to PPA";
-                            exit;;
+        -ds | --debSource   ) buildDebSource;
+                                exit;;
+        -db | --debBinary   ) buildDebBinary;
+                                exit;;
+        -c  | --clean       ) clean;
+                                exit;;
+        -d  | --debug       ) debug;
+                                exit;;
+        -ul | --update-list ) updateGFList;
+                                exit;;
+        -u | --uploadPPA    ) uploadPPA;
+                                exit;;
+                          * ) echo -e "Usage:\n-ds | --debSource:\t Generates debian source file\n-db | --debBinary:\t Generates debian binary file\n-sn | --snap:\t\t Generates Snap Package\n-c | --clean:\t\t Cleans directory, removes all build files etc\n-u | --uploadPPA:\t Uploads source file to PPA";
+                                exit;;
     esac
 fi
 echo -e "Usage:\n-ds | --debSource:\t Generates debian source file\n-db | --debBinary:\t Generates debian binary file\n-sn | --snap:\t Generates Snap Package\n-c | --clean:\t\t Cleans directory, removes all build files etc\n-u | --uploadPPA:\t Uploads source file to PPA";
